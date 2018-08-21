59807342e93a43baa349160fc5fc874bb6338306
James Fenn <18jafenn90@gmail.com>
2018-08-21 14:17:25 -0400

    added ok/down status labels

--- app/src/main/java/me/jfenn/cronhubclient/data/item/MonitorItem.java
+++ app/src/main/java/me/jfenn/cronhubclient/data/item/MonitorItem.java
@@ -1,5 +1,8 @@
 package me.jfenn.cronhubclient.data.item;
 
+import android.graphics.drawable.Drawable;
+import android.support.v4.content.ContextCompat;
+import android.support.v4.graphics.drawable.DrawableCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.TextView;
@@ -24,15 +27,24 @@ public class MonitorItem extends Item<MonitorItem.ViewHolder> {
     @Override
     public void bind(ViewHolder holder) {
         holder.title.setText(monitor.name);
+
+        holder.status.setText(String.format(holder.itemView.getContext().getString(R.string.format_status),
+                holder.itemView.getContext().getString(monitor.status.equals("up") ? R.string.title_status_up : R.string.title_status_down)));
+
+        Drawable background = DrawableCompat.wrap(holder.status.getBackground());
+        DrawableCompat.setTint(background, ContextCompat.getColor(holder.itemView.getContext(), monitor.status.equals("up") ? R.color.colorPositive : R.color.colorNegative));
+        holder.status.setBackground(background);
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
 
         private TextView title;
+        private TextView status;
 
         public ViewHolder(View v) {
             super(v);
             title = v.findViewById(R.id.title);
+            status = v.findViewById(R.id.status);
         }
     }
 
